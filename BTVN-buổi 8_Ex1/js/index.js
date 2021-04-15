bc684518b2d8cbbe75c003aee91c125f22ef93af
giang00yb <ggvu2017@gmail.com>
2021-04-15 20:00:07 +0700

    Chung Bien commit

--- BTVN-buổi 8_Ex1/js/index.js
+++ BTVN-buổi 8_Ex1/js/index.js
@@ -1,15 +1,18 @@
 let count = 0;
 function sub() {
     let number = document.getElementById("number");
-    number.innerText = count--;
+    count--;
+    number.innerText = count;
 }
 
 function add() {
     let number = document.getElementById("number");
-    number.innerText = count++;
+    count++;
+    number.innerText = count;
 }
 
 function reset(){
     let number = document.getElementById("number");
     number.innerText = count=0;
 }
+console.log(1);
